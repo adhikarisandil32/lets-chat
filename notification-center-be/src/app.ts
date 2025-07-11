d2738b0bad74799cc69923283c72bd4fdcffddb1
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-07-12 03:18:20 +0545

    refactor API_PREFIX before app.ts

--- notification-center-be/src/app.ts
+++ notification-center-be/src/app.ts
@@ -4,16 +4,13 @@ import cors from "cors";
 import { config } from "@src/config/config";
 
 const app = express();
-const API_PREFIX = config.API_PREFIX?.startsWith("/")
-  ? config.API_PREFIX
-  : `/${config.API_PREFIX ?? ""}`;
 
 app.use(cors({ origin: config.ALLOWED_CORS_ORIGINS ?? false }));
 app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get(`${API_PREFIX}`, (req, res) => {
+app.get(`${config.API_PREFIX}`, (req, res) => {
   res.json({ Test: "success" });
   return;
 });
